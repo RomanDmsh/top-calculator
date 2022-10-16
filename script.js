e6b9b61827c5a4ff621a74e64a1bd8d673d8f9c6
RomanDmsh <the_seeker@ukr.net>
2022-10-16 17:49:35 +0300

    Added basic math operations functionality

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
 const display = document.querySelector('input');
-const buttons = Array.from(document.querySelectorAll('button'));
-buttons.forEach((item) => {
+
+const numberButtons = document.querySelectorAll('.numbers button');
+numberButtons.forEach((item) => {
+	item.addEventListener('click', () => {
+		displayText(item.textContent);
+	});
+});
+const operationButtons = document.querySelectorAll('.operations button');
+let operationType = '';
+operationButtons.forEach((item) => {
 	if (item.textContent != '=') {
 		item.addEventListener('click', () => {
-			displayText(item.textContent);
+			if (operationType === '') {
+				operationType = item.textContent;
+				displayText(item.textContent);
+			} else {
+				displayResult(calculate(operationType));
+				operationType = '';
+			}
 		});
 	}
 });
 
 function displayText(text) {
 	display.value += text;
+}
+function displayResult(text) {
+	display.value = text;
+}
+
+function calculate(typeOfOperation) {
+	let raw = display.value;
+	let numbers = raw.split(typeOfOperation);
+	numbers.forEach((item, index) => {
+		item = +item;
+		numbers[index] = item;
+	});
+	if (numbers[1] === 0) {
+		numbers[1] = numbers[0];
+	}
+	switch (typeOfOperation) {
+		case '+':
+			return numbers[0] + numbers[1];
+		case '-':
+			return numbers[0] - numbers[1];
+		case '*':
+			return numbers[0] * numbers[1];
+		case '/':
+			return numbers[0] / numbers[1];
+	}
 }
